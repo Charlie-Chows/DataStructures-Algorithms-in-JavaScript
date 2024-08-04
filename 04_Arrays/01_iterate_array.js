42068d6e0017a66e2860d4f027b5e82848572e5f
Charlie-Chows <sujithkasthuri247@gmail.com>
2024-08-04 15:08:03 +0530

    iterating Array

---
+++ 04_Arrays/01_iterate_array.js
@@ -0,0 +1,9 @@
+function arrayLength ( arr , n ) {
+    for ( let i = 0; i < n; i++ ){
+            arr[i];
+    }
+    return arr;
+}
+
+let arr = [ 10, 20, 30, 40, 50 ];
+console.log( arrayLength (arr , arr.length ));
